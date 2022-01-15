586430819cecc2f75929dc16119ad6a3493f92f0
root <55458737+BryanCL01@users.noreply.github.com>
2022-01-15 15:37:19 -0800

    welcomecontent change

--- frontend/src/components/WelcomeContent.js
+++ frontend/src/components/WelcomeContent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../App.css';
 import { Container, Row, Col, Button, Form} from 'react-bootstrap'
@@ -28,39 +27,6 @@ function App() {
             <Button variant="secondary" type="submit">Sign Up</Button>    
           </Form>
         </Container>
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Row, Col, Button, Form } from "react-bootstrap";
-
-function App() {
-  return (
-    <div>
-      <Container>
-        <Form>
-          <Row>
-            <Col>
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                  type="email"
-                  placeholder="example@email.com"
-                ></Form.Control>
-              </Form.Group>
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="8 characters long"
-                ></Form.Control>
-              </Form.Group>
-            </Col>
-          </Row>
-          <Button variant="secondary" type="submit">
-            Sign Up
-          </Button>
-        </Form>
-      </Container>
->>>>>>> b5ee7b77327ea5266db9498dbe4354672096fa36
     </div>
   );
 }
