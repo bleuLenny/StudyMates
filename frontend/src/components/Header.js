c6770af4a8b5f5870094894e06a3e72811064720
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 13:21:45 -0800

    create proptypes, heading style, default prop for Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,23 @@
+import PropTypes from 'prop-types'
+
 const Header = ({ title }) => {
     return (
         <header>
-            <h1>{title}</h1>
+            <h1 style={headingStyle}>{title}</h1>
         </header>
     )
 }
 
 Header.defaultProps = {
     title: 'Study Mates',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'white',
+    backgroundColor: 'navy',
 }
 export default Header
