376832ac2bc97295eba82abc9eba32e9018b30fb
Kitty Laptop <kitty98low@gmail.com>
2022-01-15 14:09:37 -0800

    move welcome nav and content into separate components

---
+++ frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { WelcomeNav } from "../components/WelcomeNav";
+import { WelcomeContent } from "../components/WelcomeContent";
+
+export const Welcome = () => {
+  return (
+    <div>
+      <WelcomeNav />
+      <WelcomeContent />
+    </div>
+  )};
