ad11320210193172a8173ebf03b3ae197871e9d3
Kitty Laptop <kitty98low@gmail.com>
2022-01-15 13:38:42 -0800

    create welcome.js component, add root colors, testing in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container, Row, Col, Button, Form, Navbar, Nav } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Container>
+        <Navbar expand="lg" variant="light" bg="light" fixed="top">
+          <Container>
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          </Container>
+        </Navbar>
+      </Container>
+
+        <Container>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="example@email.com"></Form.Control>
+              </Form.Group>    
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="8 characters long"></Form.Control>
+              </Form.Group>    
+            </Col>
+          </Row>
+          <Button variant="secondary" type="submit">Sign Up</Button>    
+        </Form>
+        </Container>
       </header>
     </div>
   );
