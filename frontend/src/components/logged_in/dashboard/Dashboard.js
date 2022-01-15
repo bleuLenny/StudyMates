a8ad9c72d789896ca053124849e74c00eb0751db
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 14:46:01 -0800

    add Header.js to Dashboard.js

--- frontend/src/components/logged_in/dashboard/Dashboard.js
+++ frontend/src/components/logged_in/dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Dashboard = ({ username }) => {
   return (
     <div>
       <Header username={username} />
+      <Card></Card>
     </div>
   );
 };
--- frontend/src/components/logged_in/dashboard/Header.js
+++ frontend/src/components/logged_in/dashboard/Header.js
@@ -1,18 +1,37 @@
-const Header = ({ title, username}) => {
-  return (
-    <header>
-      <h1>Home</h1>
-      <h1>{title}</h1>
-      <h1>Hello {username}</h1>
-      <form action="/study">
-        <input></input>
-        <button>Submit</button>
-      </form>
-    </header>
-  );
-};
+import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+
+const Header = ({ title, username }) => {
+    return (
+        <Navbar expand="lg" variant="light" bg="info" sticky="top">
+            <Navbar.Brand href="#">
+            <h1>{title}</h1>
+            <Nav>
+                <NavDropdown title="Study Groups">
+                    <NavDropdown.Item href='search'>Search Groups</NavDropdown.Item>
+                    <NavDropdown.Item href='mygroups'>View {username} Groups</NavDropdown.Item>
+                    <NavDropdown.Item href='create'>Create Group</NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href='logout'>Logout</Nav.Link>
+            </Nav>
+            </Navbar.Brand>
+        </Navbar>
+    )
+}
 
 Header.defaultProps = {
-  title: "Study Mates",
-};
-export default Header;
+    title: 'StudyMates',
+    user: 'User'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    user: PropTypes.string
+}
+
+// const headingStyle = {
+//     color: 'white',
+//     backgroundColor: 'navy',
+// }
+export default Header
