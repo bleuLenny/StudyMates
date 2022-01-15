776d6991956907978a434d77ce3d008a0e9c9ce4
Kitty Laptop <kitty98low@gmail.com>
2022-01-15 14:19:44 -0800

    set up page router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,10 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Row, Col, Button, Form, Navbar, Nav } from 'react-bootstrap'
+import { Routes } from "./Routes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <Container class="justify-content-around">
-          <Navbar expand="lg" variant="light" bg="light" fixed="top">
-            <Container>
-              <Navbar.Brand href="#">StudyMate</Navbar.Brand>
-              <Nav.Link href="#about">About Us</Nav.Link>
-              <Nav.Link href="#support">Support Us</Nav.Link>
-              <Button>Log In</Button>
-            </Container>
-          </Navbar>
-        </Container>
-
-        <Container>
-          <Form>
-            <Row>
-              <Col>
-                <Form.Group controlId="formEmail">
-                  <Form.Label>Email Address</Form.Label>
-                  <Form.Control type="email" placeholder="example@email.com"></Form.Control>
-                </Form.Group>    
-                <Form.Group controlId="formPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="8 characters long"></Form.Control>
-                </Form.Group>    
-              </Col>
-            </Row>
-            <Button variant="secondary" type="submit">Sign Up</Button>    
-          </Form>
-        </Container>
-      </header>
-
+      <Routes />
     </div>
   );
 }
