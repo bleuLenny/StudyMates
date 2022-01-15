2766b46a11462b09897a65840f386dd2d006e38a
Alexander Amog <alex.amog@hotmail.com>
2022-01-15 13:56:34 -0800

    Added nav bar for dashboard

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,17 @@
 const Header = ({ title }) => {
-    return (
-        <header>
-            <h1>{title}</h1>
-        </header>
-    )
-}
+  return (
+    <header>
+      <h1>Home</h1>
+      <h1>{title}</h1>
+      <form action="/study">
+        <input></input>
+        <button>Submit</button>
+      </form>
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: 'Study Mates',
-}
-export default Header
+  title: "Study Mates",
+};
+export default Header;
