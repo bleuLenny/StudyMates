5f52b3edd981c0d836debdc9e08babb56422a8c4
Kitty Laptop <kitty98low@gmail.com>
2022-01-15 13:56:49 -0800

    set up basic welcome page nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,30 +7,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Container>
-        <Navbar expand="lg" variant="light" bg="light" fixed="top">
-          <Container>
-            <Navbar.Brand href="#">Navbar</Navbar.Brand>
-          </Container>
-        </Navbar>
-      </Container>
+
+        <Container class="justify-content-around">
+          <Navbar expand="lg" variant="light" bg="light" fixed="top">
+            <Container>
+              <Navbar.Brand href="#">StudyMate</Navbar.Brand>
+              <Nav.Link href="#about">About Us</Nav.Link>
+              <Nav.Link href="#support">Support Us</Nav.Link>
+              <Button>Log In</Button>
+            </Container>
+          </Navbar>
+        </Container>
 
         <Container>
-        <Form>
-          <Row>
-            <Col>
-              <Form.Group controlId="formEmail">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="example@email.com"></Form.Control>
-              </Form.Group>    
-              <Form.Group controlId="formPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="8 characters long"></Form.Control>
-              </Form.Group>    
-            </Col>
-          </Row>
-          <Button variant="secondary" type="submit">Sign Up</Button>    
-        </Form>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control type="email" placeholder="example@email.com"></Form.Control>
+                </Form.Group>    
+                <Form.Group controlId="formPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="8 characters long"></Form.Control>
+                </Form.Group>    
+              </Col>
+            </Row>
+            <Button variant="secondary" type="submit">Sign Up</Button>    
+          </Form>
         </Container>
       </header>
     </div>
