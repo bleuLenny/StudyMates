ffc6cf26de1b870c6a33dc0fb9e4e14b4ae32990
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 15:47:02 -0800

    create footer

---
+++ frontend/src/components/logged_in/dashboard/Footer.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+const Footer = () => {
+    return (
+        <Footer className='footer'>
+            <div className='container'>Lovingly made by team BAAK End</div>
+        </Footer>
+    )
+}
+
+
+
+export default Footer
