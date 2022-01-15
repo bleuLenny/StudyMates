f44c25e3e5136b7a3445e24304d4ee7ae376d974
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 15:11:00 -0800

    Add welcome Card, stats, groups

--- frontend/src/components/logged_in/dashboard/Dashboard.js
+++ frontend/src/components/logged_in/dashboard/Dashboard.js
@@ -1,12 +1,37 @@
 import React from "react";
 import Header from "./Header";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './Dashboard.css'
+import { Breadcrumb, Card } from "react-bootstrap";
 
 const Dashboard = ({ username }) => {
   return (
     <div>
       <Header username={username} />
-      <Card></Card>
+      <Card className="Dashboard-Welcome">
+        <Card.Body>
+          <Card.Title>
+            Welcome {username}!
+          </Card.Title>
+          <Card>
+            <Card.Body>
+            <Card.Title>
+              {username}'s Stats
+            </Card.Title>
+            <Card.Text>
+              some stats here
+            </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>
+                Upcoming Study Sessions
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
