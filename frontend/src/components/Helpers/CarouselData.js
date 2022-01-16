ef9909ae7bfc90e3a4f00939ccad9f41413db638
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-16 00:00:36 -0800

    substitute data for carousel

---
+++ frontend/src/components/Helpers/CarouselData.js
@@ -0,0 +1,8 @@
+
+import Gray from '../../../public/gray-bg.jpg'
+
+export const images = [
+    { groupname: "Math Club", class: "Math", time: "4:00PM", day: "Friday", info: "Here is some info about our group", img: City },
+    { groupname: "Geography Buddies", class: "Geography", time: "3:30PM", day: "Thursday", info: "Here is some info about our group", img: City },
+    { groupname: "Python Practice", class: "Object Oriented Programming", time: "5:00PM", day: "Monday", info: "Here is some info about our group", img: City }
+]
