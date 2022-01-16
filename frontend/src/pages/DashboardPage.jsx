6effc9f737d0f00b7f8b6863e59b2dc52977d4e8
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 16:09:29 -0800

    create Dashboard page

---
+++ frontend/src/pages/DashboardPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "../components/logged_in/dashboard/Dashboard";
+
+
+export const DashboardPage = ({ username }) => {
+  return (
+    <div>
+      <Dashboard />
+
+    </div>
+  );
+};
+
+export default DashboardPage;
