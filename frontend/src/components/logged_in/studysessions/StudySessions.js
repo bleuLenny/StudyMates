5456f2447f0910e2461298d19e18698d862b5a9a
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 21:07:37 -0800

    attempt card carosel

--- frontend/src/components/logged_in/studysessions/StudySessions.js
+++ frontend/src/components/logged_in/studysessions/StudySessions.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./StudySessions.css";
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import { Card } from "react-bootstrap";
 
-function StudySessions() {
+const StudySessions = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <Carousel.Caption>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title><h3>First slide label</h3></Card.Title>
+              <Card.Text>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+            </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <Carousel.Caption>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title><h3>First slide label</h3></Card.Title>
+              <Card.Text>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+            </Carousel.Caption>
+
+      </Carousel.Item>
+      <Carousel.Item>
+
+        <Carousel.Caption>
+        <Carousel.Caption>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title><h3>First slide label</h3></Card.Title>
+              <Card.Text>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+            </Carousel.Caption>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
-}
+};
 
 export default StudySessions;
