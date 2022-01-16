36041aaf28a31ff89999629fe9d776e350c4f738
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-16 10:44:29 -0800

    add read page

---
+++ frontend/src/pages/CRUD/ReadPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import <Read></Read> from "../../components/CRUD/Create";
+// import PropTypes from 'prop-types';
+
+
+export const CreatePage = ({ username }) => {
+  return (
+    <div>
+      <Create />
+
+    </div>
+  );
+};
+
+export default CreatePage;
