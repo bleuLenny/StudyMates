ed42a71aee534b6e4c303c2688f9e8d9aae0e569
root <55458737+BryanCL01@users.noreply.github.com>
2022-01-16 02:52:32 -0800

    saerch-pages update #2 - search by list

--- frontend/src/components/logged_in/dashboard/Search.js
+++ frontend/src/components/logged_in/dashboard/Search.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import {React, useState} from 'react'
 import Dashboard from './Dashboard'
 import Header from './Header'
+import axios from 'axios';
 
 export default function Search() {
+    const [courses, setCourses] = useState([]);
+    axios.get("/api/list")
+        .then((val) => {
+            setCourses(val);
+            console.log(val);
+        });
+
     return (
         <div>
             <Header/>
