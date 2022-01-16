659b82590918cf4a37d9fe4a53caaee1db6c99b0
Kitty Laptop <kitty98low@gmail.com>
2022-01-15 18:09:55 -0800

    login page css

--- frontend/src/components/general/Login.jsx
+++ frontend/src/components/general/Login.jsx
@@ -1,47 +1,56 @@
 import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import "./Login.css";
 import WelcomeNav from "../WelcomeNav";
 const Test = () => {
   return (
     <div>
       <WelcomeNav />
-      <div className="container">
-        <form action="/auth/login" method="POST">
-          <div className="form-group">
-            <h3 className="text-center">Login</h3>
-            <label>Email address</label>
-            <input
-              type="email"
-              name="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
+      <Row className="g-0">
+
+        <Col className="login-content"></Col>
+        <Col>
+          <div className="container login-content">
+            <form action="/auth/login" method="POST" className="login-form">
+              <div className="form-group">
+                <h3 className="text-center title">Login</h3>
+                <label>Email address</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" className="form-text text-muted">
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+            <p className="mt-4">
+              New User? <Link to="/auth/register">Register</Link>
+            </p>
+            <p>
+              Forgot Password? <Link to="/auth/forgot">Reset</Link>
+            </p>
           </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              name="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
-        <p className="mt-4">
-          New User? <Link to="/auth/register">Register</Link>
-        </p>
-        <p>
-          Forgot Password? <Link to="/auth/forgot">Reset</Link>
-        </p>
-      </div>
+        </Col>
+        <Col className="login-content"></Col>
+      </Row>
     </div>
   );
 };
