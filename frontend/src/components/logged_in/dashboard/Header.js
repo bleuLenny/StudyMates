e103fd914f1e1d2fc86ccfa829c212c2427c6cd2
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-15 19:51:51 -0800

    adjust Footer width, add container to header

--- frontend/src/components/logged_in/dashboard/Header.js
+++ frontend/src/components/logged_in/dashboard/Header.js
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 
 const Header = ({ title, username }) => {
     return (
         <Navbar expand="lg" variant="light" bg="info" sticky="top">
+            <Container>
             <Navbar.Brand href="#">
             <h1> {title}</h1>
             </Navbar.Brand>
@@ -14,9 +15,11 @@ const Header = ({ title, username }) => {
                     <NavDropdown.Item href='mygroups'>View {username} Groups</NavDropdown.Item>
                     <NavDropdown.Item href='create'>Create Group</NavDropdown.Item>
                 </NavDropdown>
+                
                 <Nav.Link href='logout'>Logout</Nav.Link>
+                
             </Nav>
-            
+            </Container>
         </Navbar>
     )
 }
