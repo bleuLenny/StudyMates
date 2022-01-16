50d1dac3251912ff2d81e63d7bf22c66efc68b1b
Amanda Kuiack <akuiack@my.bcit.ca>
2022-01-16 00:22:06 -0800

    add carousel bg

--- frontend/src/components/logged_in/studysessions/Carousel.js
+++ frontend/src/components/logged_in/studysessions/Carousel.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Carousel.css'
 
+import { images } from '../../Helpers/CarouselData'
+
 function Carousel() {
+    const [currGroup, setcurrGroup] = useState(0)
     return (
         <div className='carousel'>
-            <div className='carousel-inner'>
-
+            <div className='carousel-inner' style={{backgroundImage: `url(${images[currGroup].img})`}}>
+                <div className='left'></div>
+                <div className='center'></div>
+                <div className='right'></div>
             </div>
         </div>
     )
