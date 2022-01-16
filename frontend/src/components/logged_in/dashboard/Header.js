56ffd1d3f76f660c5597c56f5be24abae748125b
Alexander Amog <alex.amog@hotmail.com>
2022-01-16 02:34:50 -0800

    Fixed minor bugs

--- frontend/src/components/logged_in/dashboard/Header.js
+++ frontend/src/components/logged_in/dashboard/Header.js
@@ -2,57 +2,39 @@ import axios from "axios";
 import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.css";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
-import { Navigate } from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import "./Header.css";
 const Header = ({ title, username }) => {
-  
   const logOutButton = () => {
     axios.get("/auth/logout");
-    <Navigate to="/" />;
   };
-    return (
-        <Navbar expand="lg" variant="dark" bg="dark" sticky="top">
-            <Container>
-            <Navbar.Brand href="#">
-            <h1> {title}</h1>
-            </Navbar.Brand>
-            <Nav>
-                <NavDropdown title="Study Groups">
-                    <NavDropdown.Item href='search'>Search Groups</NavDropdown.Item>
-                    <NavDropdown.Item href='mygroups'>View {username} Groups</NavDropdown.Item>
-                    <NavDropdown.Item href='create'>Create Group</NavDropdown.Item>
-                </NavDropdown>
-                
-                <Nav.Link onClick={logOutButton}>Logout</Nav.Link>
-                
-            </Nav>
-            </Container>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar expand="lg" variant="dark" bg="dark" sticky="top">
+      <Container>
+        <Navbar.Brand>
+          <h1> {title}</h1>
+        </Navbar.Brand>
+        <Nav>
+          <NavDropdown title="Study Groups">
+            <NavDropdown.Item>
+              <Link to="create">Create a group</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="mygroups">View your Groups</Link>
+            </NavDropdown.Item>
+            <NavDropdown.Item>
+              <Link to="search">Search for a group</Link>
+            </NavDropdown.Item>
+          </NavDropdown>
 
-  
-//   return (
-//     <Navbar expand="lg" variant="light" bg="info" sticky="top">
-//       <Container>
-//         <Navbar.Brand href="#">
-//           <h1> {title}</h1>
-//         </Navbar.Brand>
-//         <Nav>
-//           <NavDropdown title="Study Groups">
-//             <NavDropdown.Item href="search">Search Groups</NavDropdown.Item>
-//             <NavDropdown.Item href="mygroups">
-//               View {username} Groups
-//             </NavDropdown.Item>
-//             <NavDropdown.Item href="create">Create Group</NavDropdown.Item>
-//           </NavDropdown>
-
-//           <Nav.Link onClick={logOutButton}>Logout</Nav.Link>
-//         </Nav>
-//       </Container>
-//     </Navbar>
-//   );
-// };
+          <Nav.Link onClick={logOutButton}>
+            <Link to="/">Logout</Link>
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 Header.defaultProps = {
   title: "StudyMates",
@@ -63,49 +45,4 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
   user: PropTypes.string,
 };
-
-// const headingStyle = {
-//     color: 'white',
-//     backgroundColor: 'navy',
-// }
 export default Header;
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import { Link } from "react-router-dom";
-
-// const Header = () => {
-//   return (
-//     <div className="App">
-//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//         <Link className="navbar-brand" to="/">
-//           StudyMate
-//         </Link>
-//         <button
-//           className="navbar-toggler"
-//           type="button"
-//           data-toggle="collapse"
-//           data-target="#navbarNavAltMarkup"
-//           aria-controls="navbarNavAltMarkup"
-//           aria-expanded="false"
-//           aria-label="Toggle navigation"
-//         >
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-//           <div className="navbar-nav">
-//             <Link className="nav-item nav-link" to="/">
-//               Home
-//             </Link>
-//             <Link className="nav-item nav-link" to="/about">
-//               About Us
-//             </Link>
-//             <Link className="nav-item nav-link" to="/login">
-//               Login
-//             </Link>
-//           </div>
-//         </div>
-//       </nav>
-//     </div>
-//   );
-// };
-
-// export default Header;
